c71bc804cdff195cc9e2ca2232681745895cb648
vargasgustavo <gustavo.neumann.v@gmail.com>
2023-09-14 20:20:14 -0300

    revisão prova

--- aula05/ex05.cpp
+++ aula05/ex05.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void antesDepois(int valor, int *ant, int *suc){
+    *ant = valor - 1;
+    *suc = valor + 1;
+}
+
 int main(){
-    
+    int a, b, c;
+    cout << "Digite um valor ";
+    cin >> a;
+    antesDepois(a, &b, &c);
+    cout << "Valor digitado " << a << endl;
+    cout << "Antecessor " << b << endl;
+    cout << "Sucessor " << c << endl;
+    return 0;
 }
