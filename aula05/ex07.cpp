db5d88558c3800f2bca6eeec1fad8cbcb1a2aafa
vargasgustavo <gustavo.neumann.v@gmail.com>
2023-09-14 20:41:25 -0300

    revisão prova 1

--- aula05/ex07.cpp
+++ aula05/ex07.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main(){
-    
+    int vet[3];
+    int i;
+    for (i = 0; i < 3; i++){
+        cout << "Digite um inteiro ";
+        cin >> vet[i];
+    }
+    // Impressão
+    for (i = 0; i < 3; i++){
+        cout << vet[i];
+    }
+
+    return 0;
+
 }
