cc8ad4f4ed966eec0220bc2733d996ad993b0054
vargasgustavo <gustavo.neumann.v@gmail.com>
2023-09-14 20:30:11 -0300

    Revisão

--- aula05/ex06.cpp
+++ aula05/ex06.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void metaDobro(int valor, int *met, int *dob){
+    *met = valor / 2;
+    *dob = valor * 2;
+}
+
 int main(){
-    
+    int a, b, c;
+    cout << "Digite um valor ";
+    cin >> a;
+    metaDobro(a, &b, &c);
+    cout << endl << "Valor digitado " << a << endl;
+    cout << "Metade " << b << endl;
+    cout << "Dobro " << c << endl;
+    return 0;
 }
