ea6715cbaca919d36ea6dbaadca1971ee70b12ce
vargasgustavo <gustavo.neumann.v@gmail.com>
2023-10-19 19:43:45 -0300

    arquivos em c

--- C/aula00/ex02.c
+++ C/aula00/ex02.c
@@ -2,5 +2,17 @@
 #include <string.h>
 
 int main(){
-    
+    FILE *f;
+    f = fopen("arquivo01.txt", "w");
+    if (f == NULL){
+        printf("Erro abertura arquivo");
+        return -1;
+    }
+    char texto[50] = "Meu primeiro programa com arquivo em C";
+    int i;
+    for (i = 0; i < strlen(texto); i++){
+        fputc(texto[i], f);
+    }
+    printf("Tamanho do texto: %d", strlen(texto));
+    fclose(f); return 0;
 }
--- C/aula00/ex03.c
+++ C/aula00/ex03.c
@@ -2,5 +2,17 @@
 #include <string.h>
 
 int main(){
-    
+    FILE *f;
+    f = fopen("arquivo01.txt", "r");
+    if (f == NULL){
+        printf("Erro abertura arquivo");
+        return -1;
+    }
+    char letra;
+    int i;
+    for (i = 0; i < 38; i++){
+        letra = fgetc(f);
+        printf("%c", letra);
+    }
+    printf("\n"); (f); return 0;
 }
